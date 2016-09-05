c753f6fde181d6e97ebe5d625d1f49888b66d878
Klaus Kazlauskas <klausmachado@gmail.com>
2016-09-05 14:23:30 -0300

    First commit

---
+++ app/src/main/java/br/com/klauskpm/ssa_becodohamburguer/MainActivity.java
@@ -0,0 +1,17 @@
+package br.com.klauskpm.ssa_becodohamburguer;
+
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
